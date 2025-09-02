8422968433738c3898dcb00d2ad95a30230be95f
suwaiphyoe-beatriz <161490401+suwaiphyoe-beatriz@users.noreply.github.com>
2025-09-02 20:56:06 +0300

    Delete week3/javascript-concepts-lab directory

--- week3/javascript-concepts-lab/convertingTem.js
+++
@@ -1,6 +0,0 @@
-const temperaturesCelsius = [0, 15, 30, 45];
-
-const temperaturesKelvin = temperaturesCelsius.map((celsius) => celsius + 273.15);
-
-console.log('Celsius Temperatures:', temperaturesCelsius);
-console.log('Kelvin Temperatures:', temperaturesKelvin);
--- week3/javascript-concepts-lab/map.js
+++
@@ -1,4 +0,0 @@
-const numbers = [2, 4, 6, 8, 10];
-const doubledNumbers = numbers.map((num) => num * 2);
-console.log('Original Numbers:', numbers);
-console.log('Doubled Numbers:', doubledNumbers);
