c9de43962f8d779c8a7417ce1c5e299afe397c7e
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-09-13 20:46:42 +0300

    fe-react-activity2 (Mon-am)

---
+++ week4/forms-intro-lab/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ContactUs from './ContactUs'
+function App() {
+  return (
+    <ContactUs />
+  );
+}
+
+export default App;
